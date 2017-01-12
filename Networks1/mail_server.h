d0dc4e091a4cd6d7ea97c0de0ac9f69c6d918c7f
noammansur <noam@optibus.co>
2017-01-12 20:35:51 +0200

    header

--- Networks1/mail_server.h
+++ Networks1/mail_server.h
@@ -1,15 +1,5 @@
 #include <stdbool.h>
 
-int init_listen(unsigned short portToListen);
-bool Authenticate(char* usersFile, int socket, char** user, char* buffer, int bufferSize);
-char** ExtractRecipients(char* recipients_string, int* amount);
-int get_msg_id(char* commandParam, int* active_user_emails);
-int getSocketByUser(char* user, Socket* sockets);
-int getSocketByfd(int fd, Socket* sockets);
-void addNewSock(int fd, Socket* sockets);
-void init_sockets(Socket* sockets);
-int getMaxFd(Socket* sockets);
-
 typedef struct email_content
 {
     char** recipients;
@@ -30,7 +20,19 @@ typedef struct socket
 {
     int fd;
     char user[MAX_USERNAME];
-    int[MAXMAILS+1] active_user_emails;
+    int active_user_emails[MAXMAILS+1];
     bool isAuth;
     bool isActive;
 }Socket;
+
+int init_listen(unsigned short portToListen);
+bool Authenticate(char* usersFile, int socket, char** user, char* buffer, int bufferSize);
+char** ExtractRecipients(char* recipients_string, int* amount);
+int get_msg_id(char* commandParam, int* active_user_emails);
+int getSocketByUser(char* user, Socket* sockets);
+int getSocketByfd(int fd, Socket* sockets);
+void addNewSock(int fd, Socket* sockets);
+void init_sockets(Socket* sockets);
+int getMaxFd(Socket* sockets);
+
+
