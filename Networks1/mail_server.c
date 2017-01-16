5b87d146d86009b7a33f8a36d293e02853eb6f2e
noammansur <noam@optibus.co>
2017-01-16 19:09:34 +0200

    try3

--- Networks1/mail_server.c
+++ Networks1/mail_server.c
@@ -155,7 +155,7 @@ int main(int argc, char* argv[])
                         }
                     }
                     printf("%s\n", buffer);
-                    if (sendall(sockets[i].fd, (char *)(&buffer)) == -1)
+                    if (sendall(sockets[i].fd, (char *)(&buffer)+1) == -1)
                     {
                         closeSocket(&sockets[i]);
                         continue;
